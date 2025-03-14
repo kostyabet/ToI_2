5bc1a0e5034edfbc58a95cbb35f729e5a4d4916b
Kostya Betenya <betenya.kostya@mail.ru>
2025-03-14 11:06:50 +0300

    fet: complete task

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace ToI_2
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -17,23 +17,327 @@
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.keyPanel = new System.Windows.Forms.Panel();
+            this.keyLengthLabel = new System.Windows.Forms.Label();
+            this.genKeyTextBox = new System.Windows.Forms.TextBox();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.genKeyLabel = new System.Windows.Forms.Label();
+            this.inputKeyLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fromFileButton = new System.Windows.Forms.Button();
+            this.inFileButton = new System.Windows.Forms.Button();
+            this.cypherTextBox = new System.Windows.Forms.TextBox();
+            this.plainTextBox = new System.Windows.Forms.TextBox();
+            this.cypherLabel = new System.Windows.Forms.Label();
+            this.plainLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.genKeyButton = new System.Windows.Forms.Button();
+            this.genKeySizeLabel = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.keyPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // keyPanel
+            // 
+            this.keyPanel.BackColor = System.Drawing.Color.PaleGreen;
+            this.keyPanel.Controls.Add(this.genKeySizeLabel);
+            this.keyPanel.Controls.Add(this.genKeyButton);
+            this.keyPanel.Controls.Add(this.keyLengthLabel);
+            this.keyPanel.Controls.Add(this.genKeyTextBox);
+            this.keyPanel.Controls.Add(this.keyTextBox);
+            this.keyPanel.Controls.Add(this.genKeyLabel);
+            this.keyPanel.Controls.Add(this.inputKeyLabel);
+            this.keyPanel.Location = new System.Drawing.Point(5, 5);
+            this.keyPanel.Name = "keyPanel";
+            this.keyPanel.Size = new System.Drawing.Size(810, 135);
+            this.keyPanel.TabIndex = 0;
+            // 
+            // keyLengthLabel
+            // 
+            this.keyLengthLabel.AutoSize = true;
+            this.keyLengthLabel.Location = new System.Drawing.Point(9, 111);
+            this.keyLengthLabel.Name = "keyLengthLabel";
+            this.keyLengthLabel.Size = new System.Drawing.Size(166, 13);
+            this.keyLengthLabel.TabIndex = 4;
+            this.keyLengthLabel.Text = "Длинна введённых состояний: ";
+            // 
+            // genKeyTextBox
+            // 
+            this.genKeyTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.genKeyTextBox.Enabled = false;
+            this.genKeyTextBox.Location = new System.Drawing.Point(408, 43);
+            this.genKeyTextBox.MaximumSize = new System.Drawing.Size(393, 60);
+            this.genKeyTextBox.MinimumSize = new System.Drawing.Size(393, 60);
+            this.genKeyTextBox.Multiline = true;
+            this.genKeyTextBox.Name = "genKeyTextBox";
+            this.genKeyTextBox.ReadOnly = true;
+            this.genKeyTextBox.Size = new System.Drawing.Size(393, 60);
+            this.genKeyTextBox.TabIndex = 3;
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.keyTextBox.Location = new System.Drawing.Point(9, 43);
+            this.keyTextBox.MaximumSize = new System.Drawing.Size(393, 60);
+            this.keyTextBox.MinimumSize = new System.Drawing.Size(393, 60);
+            this.keyTextBox.Multiline = true;
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(393, 60);
+            this.keyTextBox.TabIndex = 2;
+            this.keyTextBox.TextChanged += new System.EventHandler(this.keyTextBox_TextChanged);
+            // 
+            // genKeyLabel
+            // 
+            this.genKeyLabel.AutoSize = true;
+            this.genKeyLabel.BackColor = System.Drawing.Color.LightGreen;
+            this.genKeyLabel.Location = new System.Drawing.Point(408, 8);
+            this.genKeyLabel.MaximumSize = new System.Drawing.Size(393, 30);
+            this.genKeyLabel.MinimumSize = new System.Drawing.Size(393, 30);
+            this.genKeyLabel.Name = "genKeyLabel";
+            this.genKeyLabel.Size = new System.Drawing.Size(393, 30);
+            this.genKeyLabel.TabIndex = 1;
+            this.genKeyLabel.Text = "Сгенерированный ключ";
+            this.genKeyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputKeyLabel
+            // 
+            this.inputKeyLabel.AutoSize = true;
+            this.inputKeyLabel.BackColor = System.Drawing.Color.LightGreen;
+            this.inputKeyLabel.Location = new System.Drawing.Point(9, 8);
+            this.inputKeyLabel.MaximumSize = new System.Drawing.Size(393, 30);
+            this.inputKeyLabel.MinimumSize = new System.Drawing.Size(393, 30);
+            this.inputKeyLabel.Name = "inputKeyLabel";
+            this.inputKeyLabel.Size = new System.Drawing.Size(393, 30);
+            this.inputKeyLabel.TabIndex = 0;
+            this.inputKeyLabel.Text = "Состояние регистра (36 бита)";
+            this.inputKeyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel1.Controls.Add(this.fromFileButton);
+            this.panel1.Controls.Add(this.inFileButton);
+            this.panel1.Controls.Add(this.cypherTextBox);
+            this.panel1.Controls.Add(this.plainTextBox);
+            this.panel1.Controls.Add(this.cypherLabel);
+            this.panel1.Controls.Add(this.plainLabel);
+            this.panel1.Location = new System.Drawing.Point(5, 146);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(810, 145);
+            this.panel1.TabIndex = 1;
+            // 
+            // fromFileButton
+            // 
+            this.fromFileButton.BackColor = System.Drawing.Color.LightGreen;
+            this.fromFileButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.fromFileButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.fromFileButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkSeaGreen;
+            this.fromFileButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGreen;
+            this.fromFileButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.fromFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fromFileButton.Location = new System.Drawing.Point(9, 108);
+            this.fromFileButton.Name = "fromFileButton";
+            this.fromFileButton.Size = new System.Drawing.Size(393, 28);
+            this.fromFileButton.TabIndex = 6;
+            this.fromFileButton.Text = "Ввести исходный текст из файл";
+            this.fromFileButton.UseVisualStyleBackColor = false;
+            this.fromFileButton.Click += new System.EventHandler(this.fromFileButton_Click);
+            // 
+            // inFileButton
+            // 
+            this.inFileButton.BackColor = System.Drawing.Color.LightGreen;
+            this.inFileButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.inFileButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.inFileButton.Enabled = false;
+            this.inFileButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkSeaGreen;
+            this.inFileButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGreen;
+            this.inFileButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.inFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inFileButton.Location = new System.Drawing.Point(408, 108);
+            this.inFileButton.Name = "inFileButton";
+            this.inFileButton.Size = new System.Drawing.Size(393, 28);
+            this.inFileButton.TabIndex = 5;
+            this.inFileButton.Text = "Сохранить сгенерированный результат в файл";
+            this.inFileButton.UseVisualStyleBackColor = false;
+            this.inFileButton.Click += new System.EventHandler(this.inFileButton_Click);
+            // 
+            // cypherTextBox
+            // 
+            this.cypherTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.cypherTextBox.Enabled = false;
+            this.cypherTextBox.Location = new System.Drawing.Point(408, 43);
+            this.cypherTextBox.MaximumSize = new System.Drawing.Size(393, 60);
+            this.cypherTextBox.MinimumSize = new System.Drawing.Size(393, 60);
+            this.cypherTextBox.Multiline = true;
+            this.cypherTextBox.Name = "cypherTextBox";
+            this.cypherTextBox.ReadOnly = true;
+            this.cypherTextBox.Size = new System.Drawing.Size(393, 60);
+            this.cypherTextBox.TabIndex = 3;
+            // 
+            // plainTextBox
+            // 
+            this.plainTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.plainTextBox.Enabled = false;
+            this.plainTextBox.Location = new System.Drawing.Point(9, 43);
+            this.plainTextBox.MaximumSize = new System.Drawing.Size(393, 60);
+            this.plainTextBox.MinimumSize = new System.Drawing.Size(393, 60);
+            this.plainTextBox.Multiline = true;
+            this.plainTextBox.Name = "plainTextBox";
+            this.plainTextBox.ReadOnly = true;
+            this.plainTextBox.Size = new System.Drawing.Size(393, 60);
+            this.plainTextBox.TabIndex = 2;
+            // 
+            // cypherLabel
+            // 
+            this.cypherLabel.AutoSize = true;
+            this.cypherLabel.BackColor = System.Drawing.Color.LightGreen;
+            this.cypherLabel.Location = new System.Drawing.Point(408, 8);
+            this.cypherLabel.MaximumSize = new System.Drawing.Size(393, 30);
+            this.cypherLabel.MinimumSize = new System.Drawing.Size(393, 30);
+            this.cypherLabel.Name = "cypherLabel";
+            this.cypherLabel.Size = new System.Drawing.Size(393, 30);
+            this.cypherLabel.TabIndex = 1;
+            this.cypherLabel.Text = "Сгенерированный результат";
+            this.cypherLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plainLabel
+            // 
+            this.plainLabel.AutoSize = true;
+            this.plainLabel.BackColor = System.Drawing.Color.LightGreen;
+            this.plainLabel.Location = new System.Drawing.Point(9, 8);
+            this.plainLabel.MaximumSize = new System.Drawing.Size(393, 30);
+            this.plainLabel.MinimumSize = new System.Drawing.Size(393, 30);
+            this.plainLabel.Name = "plainLabel";
+            this.plainLabel.Size = new System.Drawing.Size(393, 30);
+            this.plainLabel.TabIndex = 0;
+            this.plainLabel.Text = "Исходный текст";
+            this.plainLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGreen;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(5, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(402, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Шифрование";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DarkSeaGreen;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGreen;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(413, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(402, 30);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Дешифрование";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // genKeyButton
+            // 
+            this.genKeyButton.BackColor = System.Drawing.Color.LightGreen;
+            this.genKeyButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.genKeyButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.genKeyButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkSeaGreen;
+            this.genKeyButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGreen;
+            this.genKeyButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.genKeyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.genKeyButton.Location = new System.Drawing.Point(408, 104);
+            this.genKeyButton.Name = "genKeyButton";
+            this.genKeyButton.Size = new System.Drawing.Size(393, 28);
+            this.genKeyButton.TabIndex = 5;
+            this.genKeyButton.Text = "Сгенерировать Ключ";
+            this.genKeyButton.UseVisualStyleBackColor = false;
+            this.genKeyButton.Visible = false;
+            this.genKeyButton.Click += new System.EventHandler(this.genKeyButton_Click);
+            // 
+            // genKeySizeLabel
+            // 
+            this.genKeySizeLabel.AutoSize = true;
+            this.genKeySizeLabel.Location = new System.Drawing.Point(408, 111);
+            this.genKeySizeLabel.Name = "genKeySizeLabel";
+            this.genKeySizeLabel.Size = new System.Drawing.Size(189, 13);
+            this.genKeySizeLabel.TabIndex = 8;
+            this.genKeySizeLabel.Text = "Длинна сгенерированного ключа: 0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(820, 331);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.keyPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LFSR";
+            this.keyPanel.ResumeLayout(false);
+            this.keyPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel keyPanel;
+        private System.Windows.Forms.Label inputKeyLabel;
+        private System.Windows.Forms.Label genKeyLabel;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.TextBox genKeyTextBox;
+        private System.Windows.Forms.Label keyLengthLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button fromFileButton;
+        private System.Windows.Forms.Button inFileButton;
+        private System.Windows.Forms.TextBox cypherTextBox;
+        private System.Windows.Forms.TextBox plainTextBox;
+        private System.Windows.Forms.Label cypherLabel;
+        private System.Windows.Forms.Label plainLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button genKeyButton;
+        private System.Windows.Forms.Label genKeySizeLabel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,264 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+
 namespace ToI_2
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private char[] goodKeys = { '0', '1' };
+
+        private string PLAINT_TEXT;
+        private string GENERATED_KEY;
+        private string CIPER_TEXT;
+
+        public MainForm()
         {
             InitializeComponent();
+            setCurrentKeyLength(0);
+        }
+
+        private void setCurrentKeyLength(int length)
+        {
+            keyLengthLabel.Text = $"Длинна введённых состояний: {length}";
+        }
+        private string getCurrentKey(string str)
+        {
+            string res = "";
+            for (int i = 0; i < str.Length; i++)
+                if (goodKeys.Contains(str[i]))
+                    res += str[i];
+            return res;
+        }
+        private (bool, string) getCurrentKeyString(string str)
+        {
+            if (str.Length == 0)
+                return (false, "Введите исходное состояние для генерации ключа!");
+            string res = getCurrentKey(str);
+            if (res.Length < 36)
+                return (false, "Введите не менее 36 состояний ключа!");
+            return (true, res);
+        }
+
+        private string getFirstBytes(string str, int bytes)
+        {
+            return str.Substring(0, bytes * 8);
+        }
+
+        private string getLastBytes(string str, int bytes)
+        {
+            return str.Substring(str.Length - bytes * 8, bytes * 8);
+        }
+
+        private void setGeneratedText(TextBox textBox, string str)
+        {
+            const int bytesToSee = 8;
+            if (str.Length > bytesToSee * 8 * 2)
+                textBox.Text = $"Перывые 8 байт: {getFirstBytes(str, bytesToSee)}\n\nПоследние 8 байт: {getLastBytes(str, bytesToSee)}";
+            else
+                textBox.Text = str;
+        }
+
+        private char[] shiftKey(char[] key)
+        {
+            for (int i = 0; i < key.Length - 1; i++)
+                key[i] = key[i + 1];
+            return key;
+        }
+        private char xorNums(char bit1, char bit2)
+        {
+            int num1 = bit1 - '0';
+            int num2 = bit2 - '0';
+            int result = num1 ^ num2;
+            char xorResult = (char)(result + '0');
+            return xorResult;
+        }
+        private char getNewValue(int[] genPos, char[] key)
+        {
+            char first;
+            char second = key[genPos[0] - 1];
+            for (var i = 1; i < genPos.Length; i++)
+            {
+                first = second;
+                second = key[genPos[i] - 1];
+                second = xorNums(first, second);
+            }
+            return second;
+        }
+        private string generateByLFSR(string register, int length)
+        {
+            char[] genKey = register.ToArray();
+            char[] currentKey = register.ToArray();
+            int[] xorPositions = { 36, 11 }; // from 1 .. 36
+
+            StringBuilder bits = new StringBuilder();
+            for (var i = 0; i < currentKey.Length; i++)
+            {
+                bits.Append(currentKey[i]);
+            }
+
+            while (bits.Length < length)
+            {
+                genKey = shiftKey(genKey);
+                genKey[genKey.Length - 1] = getNewValue(xorPositions, genKey);
+                bits.Append(genKey[genKey.Length - 1]);
+            }
+
+            return bits.ToString();
+        }
+
+        private void keyTextBox_TextChanged(object sender, EventArgs e)
+        {
+            setCurrentKeyLength(getCurrentKey(keyTextBox.Text).Length);
+        }
+
+        private void genKeyButton_Click(object sender, EventArgs e)
+        {
+            //string str;
+            //bool isCorrect;
+            //(isCorrect, str) = getCurrentKeyString(keyTextBox.Text);
+            //if (!isCorrect)
+            //{
+            //    MessageBox.Show(str, "Ошибка!");
+            //    return;
+            //}
+            // generate
+            //string key = generateByLFSR(str.Substring(0, 36));
+            //setGeneratedKey(key);
+        }
+
+        private string encryptionAlghoritm(string plain, string key)
+        {
+            int test = key.Length;
+            StringBuilder bits = new StringBuilder();
+            for (int i = 0; i < plain.Length; i++)
+            {
+                char p = plain[i];
+                char k = key[i];
+                bits.Append(xorNums(p, k));
+            }
+            return bits.ToString();
+        }
+
+        private void ENCRIPTION()
+        {
+            /* Key part */
+            string key;
+            bool isCorrect;
+            (isCorrect, key) = getCurrentKeyString(keyTextBox.Text);
+            if (!isCorrect)
+            {
+                MessageBox.Show(key, "Ошибка!");
+                return;
+            }
+
+            /* Plain text part */
+            if (PLAINT_TEXT.Length == 0)
+            {
+                MessageBox.Show("Введите исходные данные через файл.", "Ошибка!");
+                return;
+            }
+
+            /* Generate key part */
+            GENERATED_KEY = generateByLFSR(key.Substring(0, 36), PLAINT_TEXT.Length);
+            setGeneratedText(genKeyTextBox, GENERATED_KEY);
+
+            /* Cipher part */
+            CIPER_TEXT = encryptionAlghoritm(PLAINT_TEXT, GENERATED_KEY);
+            setGeneratedText(cypherTextBox, CIPER_TEXT);
+
+            /* Enables */
+            inFileButton.Enabled = true;
+            genKeySizeLabel.Text = $"Длинна сгенерированного ключа: {GENERATED_KEY.Length}";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ENCRIPTION();
+        }
+
+        private (bool, string) ReadFileAsBits(string filePath)
+        {
+            try
+            {
+                StringBuilder bits = new StringBuilder();
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    int byteRead;
+                    while ((byteRead = fs.ReadByte()) != -1)
+                    {
+                        bits.Append(Convert.ToString(byteRead, 2).PadLeft(8, '0'));
+                    }
+                }
+                if (bits.Length > 0)
+                    return (true, bits.ToString());
+
+                return (false, null);
+            }
+            catch
+            {
+                return (false, null);
+            }
+        }
+
+        private (bool, string) WriteFileAsBits(string filePath)
+        {
+            string bits = CIPER_TEXT;
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    for (int i = 0; i < bits.Length; i += 8)
+                    {
+                        string byteBits = bits.Substring(i, Math.Min(8, bits.Length - i));
+                        byte byteToWrite = Convert.ToByte(byteBits, 2);
+                        fs.WriteByte(byteToWrite);
+                    }
+                }
+
+                return (true, null);
+            }
+            catch
+            {
+                return (false, null);
+            }
+        }
+
+        private void fromFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bool status;
+                string result;
+                (status, result) = ReadFileAsBits(openFileDialog.FileName);
+                if (status) {
+                    PLAINT_TEXT = result;
+                    setGeneratedText(plainTextBox, PLAINT_TEXT);
+                    return;
+                }
+                MessageBox.Show("Данные в файле не являются корректными.", "Ошибка!");
+            }
+        }
+
+        private void inFileButton_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string str;
+                bool status;
+                (status, str) = WriteFileAsBits(saveFileDialog.FileName);
+                MessageBox.Show(status ? "Данные успешно записаны в файл." : "Ошибка при записи в файл.", status ? "Успех" : "Ошибка");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ENCRIPTION();
         }
     }
 }
